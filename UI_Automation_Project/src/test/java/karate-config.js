d3ff47e896b7f4009df98cda1daa3e5bcf822312
Shashikant Mane <smane@academian.com>
2024-06-20 01:13:12 +0530

    modify json file path

--- UI_Automation_Project/src/test/java/karate-config.js
+++ UI_Automation_Project/src/test/java/karate-config.js
@@ -1,8 +1,8 @@
 function fn() {
   var env = karate.env; // get system property 'karate.env'
-  karate.log('karate.env system property was:', env);
-  var browser = karate.properties['browser'] || 'chromedriver';
-  karate.log('the browser set is: ' + browser + ', default: "chrome"');
+  // karate.log('karate.env system property was:', env);
+  // var browser = karate.properties['browser'] || 'chromedriver';
+  // karate.log('the browser set is: ' + browser + ', default: "chrome"');
   if (!env) {
     env = 'dev';
   }
@@ -11,6 +11,10 @@ function fn() {
     browser:browser,
     myVarName: 'someValue',
     baseUrl:  'https://www.saucedemo.com/',
+    driver: {
+      type: 'chrome',
+      executable: "C://Program Files//Google//Chrome//Application//chrome.exe" // Specify ChromeDriver path if needed
+    }
     // locators: read('classpath:UI_Automation_Project/login/locators.json'),
     // testData: read('classpath:UI_Automation_Project/login/testData.json')
   }
